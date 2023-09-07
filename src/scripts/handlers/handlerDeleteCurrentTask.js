6db776d02c0f4c7776596e563fef62f1665bc148
Irina Shchetko <irina.shetko92@gmail.com>
2023-09-08 02:29:28 +0300

    feat:added function for delete current task from localstorage

--- src/scripts/handlers/handlerDeleteCurrentTask.js
+++ src/scripts/handlers/handlerDeleteCurrentTask.js
@@ -1,5 +1,13 @@
+import { getDataLocalStorage, setDataLocalStorage } from '../storage/index.js'
+
 export function handlerDeleteCurrentTask(event) {
     const task = event.target.closest('.task__body')
     const bodyBoard = document.querySelector('.boards__item-body')
+    
+    const taskTitle = task.querySelector('.task__title').textContent
     bodyBoard.removeChild(task)
+
+    const allTasks = getDataLocalStorage('tasks')
+    const updatedTasks = allTasks.filter(item => item.title !== taskTitle)
+    setDataLocalStorage('tasks', updatedTasks)
 }
